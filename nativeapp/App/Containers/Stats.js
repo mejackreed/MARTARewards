7d30a256509ee8c5b5d94b3192d5bd54646082f8
Jack Reed <phillipjreed@gmail.com>
2016-10-29 15:22:22 -0400

    add stats

--- nativeapp/App/Containers/Stats.js
+++ nativeapp/App/Containers/Stats.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import { ScrollView, Text, KeyboardAvoidingView } from 'react-native'
+import { ScrollView, Text, KeyboardAvoidingView, View, Image } from 'react-native'
 import { connect } from 'react-redux'
 // Add Actions - replace 'Your' with whatever your reducer is called :)
 // import YourActions from '../Redux/YourRedux'
-import { Metrics } from '../Themes'
+import { Metrics, Images } from '../Themes'
 // external libs
 import Icon from 'react-native-vector-icons/FontAwesome'
 import Animatable from 'react-native-animatable'
@@ -21,7 +21,26 @@ class Stats extends React.Component {
     return (
       <ScrollView style={styles.container}>
         <KeyboardAvoidingView behavior='position'>
-          <Text>Stats Container</Text>
+        <View style={styles.mainContainer}>
+          <View style={{alignItems: 'center'}}>
+            <View style={styles.section}>
+              <View style={{flexDirection: 'row'}}>
+                <Text style={{fontSize: 36}}>{Math.floor(Math.random() * (100 - 1)) + 1}</Text>
+                <Image source={Images.trainWheel} style={{width: 50, height: 50}} />
+              </View>
+            </View>
+            <View style={styles.section}>
+              <Text>Season</Text>
+              <Text>34 miles ridden</Text>
+              <Text>1,253 of 57,642</Text>
+            </View>
+            <View style={styles.section}>
+              <Text>Lifetime</Text>
+              <Text>476 miles ridden</Text>
+              <Text>11,242 of 103,642 users over lifetime</Text>
+            </View>
+          </View>
+        </View>
         </KeyboardAvoidingView>
       </ScrollView>
     )
